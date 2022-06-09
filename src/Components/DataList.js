133993c5ed6c76e228d415d419f24624676d7615
Aniksaha-ui <sahaanik1045@gmail.com>
2022-06-09 20:42:45 +0600

    pagination done and install filter package

--- src/Components/DataList.js
+++ src/Components/DataList.js
@@ -1,25 +1,55 @@
 import React, { useEffect, useState } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 
 const DataList = () => {
   const [userList, setUserList] = useState([]);
   const columns = [
     { dataField: "id", text: "Id", sort: true },
-    { dataField: "name", text: "Name", sort: true },
-    { dataField: "username", text: "Username", sort: true },
-    { dataField: "email", text: "Email", sort: true },
+    { dataField: "userId", text: "UserId", sort: true },
+    { dataField: "title", text: "Title", sort: true },
+    { dataField: "body", text: "Body", sort: true },
   ];
+
+  const pagination = paginationFactory({
+    page: 1,
+    sizePerPage: 5,
+    lastPageText: ">>",
+    firstPageText: "<<",
+    prePageText: "prev",
+    nextPageText: "next",
+    showTotal: true,
+    alwaysShowAllBtns: true,
+    onPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+    onSizePerPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+  });
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((result) => setUserList(result))
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div>
-      <BootstrapTable keyField="id" columns={columns} data={userList} />
+    <div className="mt-5">
+      <h2>Please just scroll if want to change the size of post per page.</h2>
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        columns={columns}
+        data={userList}
+        pagination={pagination}
+      />
 
       {/* <table>
         <tr>
